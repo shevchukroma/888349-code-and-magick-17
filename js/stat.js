352c333761f922bb5e963bf86d24269be1db1282
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-06-09 20:44:01 +0300

    Убирает лишнее из функции renderStatistic

--- js/stat.js
+++ js/stat.js
@@ -34,8 +34,7 @@ var getMaxElement = function(arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloudWithShadow(ctx);
 
   ctx.fillStyle = '#000';
 
