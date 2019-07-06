bc7af8db66d594c369ee2c7456319b2e5fa2ae78
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-06 20:35:40 +0300

    Делает задание четверной лекции

--- js/setup.js
+++ js/setup.js
@@ -11,7 +11,66 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var wizardsMock = [];
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var icon = document.querySelector('.setup-open-icon');
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEye = document.querySelector('.setup-wizard .wizard-eyes');
+var fireball = document.querySelector('.setup-fireball-wrap');
+var input = document.querySelector('.setup-user-name');
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+};
+
+icon.addEventListener('keydown', function (evt){
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+document.addEventListener('keydown', function(evt){
+  if (evt.target !== input) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closePopup();
+    }
+  }
+});
+
+setupClose.addEventListener('keydown', function (evt){
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupClose.addEventListener('click', function(){
+  closePopup();
+});
+
+wizardCoat.addEventListener('click', function (){
+  wizardCoat.style.fill = COAT_COLORS[getRandomInt(COAT_COLORS.length)];
+});
+
+wizardEye.addEventListener('click', function (){
+  wizardEye.style.fill = EYES_COLORS[getRandomInt(EYES_COLORS.length)];
+});
+
+fireball.addEventListener('click', function () {
+  fireball.style.backgroundColor = FIREBALL_COLORS[getRandomInt(FIREBALL_COLORS.length)];
+});
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
