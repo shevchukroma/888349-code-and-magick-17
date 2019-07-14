11df721275a73a63375cea90f750ede450fe1c34
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-14 11:37:26 +0300

    Учебный проект: в движении

---
+++ js/dialog.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var upload = document.querySelector('.upload');
+
+upload.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  popupClose.addEventListener('click', function (evt) {
+    popup.style = false;
+  });
+  var dragged = false;
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    popup.style.top = (popup.offsetTop - shift.y) + 'px';
+    popup.style.left = (popup.offsetLeft - shift.x) + 'px';    
+  };
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        popup.removeEventListener('click', onClickPreventDefault)
+      };
+      popup.addEventListener('click', onClickPreventDefault);
+    }
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);  
+});
