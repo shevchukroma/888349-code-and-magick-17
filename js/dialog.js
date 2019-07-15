0263942dd217ad9ce16318aa50827bbcc4326444
shevchukroma <46864764+shevchukroma@users.noreply.github.com>
2019-07-15 18:58:15 +0300

    Исправляет ошибку с перетаскиваниям элементов в рюкзак

--- js/dialog.js
+++ js/dialog.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var upload = document.querySelector('.upload');
+var item = document.querySelector('.setup-artifacts-cell img');
 var popup;
 
 upload.addEventListener('mousedown', function (evt) {
@@ -38,6 +39,39 @@ upload.addEventListener('mousedown', function (evt) {
       };
       popup.addEventListener('click', onClickPreventDefault);
     }
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+item.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  item.style.position = 'absolute';
+  item.style.zIndex = '5';
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  }
+
+  var onMouseMove = function (moveEvt) {
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    }
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    }
+
+    item.style.left = (item.offsetLeft - shift.x) + 'px';
+    item.style.top = (item.offsetTop - shift.y) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
   };
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
--- js/setup.js
+++ js/setup.js
@@ -41,7 +41,7 @@ function openPopup() {
 function closePopup() {
   popup.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
-  popup.style = false;
+  popup.removeAttribute('style');
 }
 
 icon.addEventListener('keydown', function (evt) {
